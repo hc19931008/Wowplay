4092c1f450f0c417f51f3a89058856492c6301c3
Ada456 <2207047327@qq.com>
2016-07-14 19:18:38 +0800

    delete test

---
+++ src/com/xtkj/wowplay/intercepter/LoginCheckIntercepter.java
@@ -0,0 +1,26 @@
+package com.xtkj.wowplay.intercepter;
+
+
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.Interceptor;
+
+/**
+ * Created by Administrator on 2016/7/13 0013.
+ */
+public class LoginCheckIntercepter implements Interceptor {
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public String intercept(ActionInvocation actionInvocation) throws Exception {
+        return null;
+    }
+}
